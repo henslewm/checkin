2afafd898fc270b78681d48a2615bed4dfd0081c
henslewm <henslewm@gmail.com>
2019-09-04 09:43:44 -0400

    Try the

--- app/src/main/java/com/example/aacheckin/MainActivity.java
+++ app/src/main/java/com/example/aacheckin/MainActivity.java
@@ -4,60 +4,101 @@ import android.Manifest;
 import android.app.Activity;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.os.Message;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.webkit.CookieSyncManager;
+import android.webkit.CookieManager;
+import android.webkit.WebChromeClient;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 
 public class MainActivity extends Activity {
-    int MY_PERMISSIONS_REQUEST_READ_CONTACTS;
+    private static int PERMISSION_REQUEST_CAMERA = 0;
+    private static int PERMISSION_REQUEST_AUDIO = 0;
+    private static final String url1 = "https://newjetnet.aa.com";
+    private static final String url2 = "https://newjetnet.aa.com/themes/jetnet-new/redirect/travel.html?cint=tp_jetnet_fif_desktop";
+    private boolean loadingFinished = true;
+    final String password = "Intruso66!";
+    final String userID = "315630";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
-        // Here, thisActivity is the current activity
-        if (ContextCompat.checkSelfPermission(MainActivity,
-                Manifest.permission.CAMERA)
-                != PackageManager.PERMISSION_GRANTED) {
+        //Check for permissions
+        CheckForCameraPermission();
+        CheckForAudioPermission();
 
-            // Permission is not granted
-            // Should we show an explanation?
-            if (ActivityCompat.shouldShowRequestPermissionRationale(MainActivity,
-                    Manifest.permission.CAMERA)) {
-                // Show an explanation to the user *asynchronously* -- don't block
-                // this thread waiting for the user's response! After the user
-                // sees the explanation, try again to request the permission.
-            } else {
-                // No explanation needed; request the permission
-                ActivityCompat.requestPermissions(MainActivity,
-                        new String[]{Manifest.permission.CAMERA},
-                        MY_PERMISSIONS_REQUEST_READ_CONTACTS);
-
-                // MY_PERMISSIONS_REQUEST_READ_CONTACTS is an
-                // app-defined int constant. The callback method gets the
-                // result of the request.
-            }
-        } else {
-            // Permission has already been granted
-        }
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // use cookies to remember a logged in status
-
-
         //Trying to automate the website...
-        WebView myWebView = new WebView(getApplicationContext());
+        WebView myWebView = new WebView(this);
         setContentView(myWebView);
         WebSettings webSettings = myWebView.getSettings();
         webSettings.setJavaScriptEnabled(true);
         webSettings.setJavaScriptCanOpenWindowsAutomatically(true);
+        webSettings.setSupportMultipleWindows(true);
         webSettings.setDomStorageEnabled(true);
-        myWebView.loadUrl("https://newjetnet.aa.com");
+        webSettings.setAppCacheEnabled(true);
+        webSettings.setDatabaseEnabled(true);
+        webSettings.setAllowContentAccess(true);
+        webSettings.setLoadsImagesAutomatically(true);
+
+        //Enable Cookies
+        // use cookies to remember a logged in status
+        if (android.os.Build.VERSION.SDK_INT >= 21) {
+            CookieManager.getInstance().setAcceptThirdPartyCookies(myWebView, true);
+        } else {
+            CookieManager.getInstance().setAcceptCookie(true);
+        }
+
+        //Set WebViewClient
+        myWebView.setWebViewClient(new WebViewClient());
+        myWebView.setWebChromeClient(new WebChromeClient() {
+
+
+            @Override
+            public boolean onCreateWindow(WebView view, boolean isDialog,
+                                          boolean isUserGesture, Message resultMsg) {
+
+
+
+                WebView newWebView = new WebView(WebpageActivity.this);
+                newWebView.getSettings().setJavaScriptEnabled(true);
+                newWebView.getSettings().setSupportZoom(true);
+                newWebView.getSettings().setBuiltInZoomControls(true);
+                newWebView.getSettings().setPluginState(WebSettings.PluginState.ON);
+                newWebView.getSettings().setSupportMultipleWindows(true);
+                view.addView(newWebView);
+                WebView.WebViewTransport transport = (WebView.WebViewTransport) resultMsg.obj;
+                transport.setWebView(newWebView);
+                resultMsg.sendToTarget();
+
+                newWebView.setWebViewClient(new WebViewClient() {
+                    @Override
+                    public boolean shouldOverrideUrlLoading(WebView view, String url) {
+                        view.loadUrl(url);
+                        return true;
+                    }
+                });
+
+                return true;
+            }
+        });
+
+        //Open Website
+        Log.i("onCreate", "myWebView.loadUrl(url1);");
+        myWebView.loadUrl(url1);
+
+
+       // Log.i("onCreate", "myWebView.loadUrl(url2);");
+       //myWebView.loadUrl(url2);
+
 
     }
 
@@ -82,4 +123,63 @@ public class MainActivity extends Activity {
 
         return super.onOptionsItemSelected(item);
     }
+
+    //Ask for permission to use camera and microphone
+    private void CheckForCameraPermission(){        // Here, thisActivity is the current activity
+        if (ContextCompat.checkSelfPermission(MainActivity.this,
+                Manifest.permission.CAMERA)
+                != PackageManager.PERMISSION_GRANTED) {
+
+            // Permission is not granted
+            // Should we show an explanation?
+            if (ActivityCompat.shouldShowRequestPermissionRationale(MainActivity.this,
+                    Manifest.permission.CAMERA)) {
+                // Show an explanation to the user *asynchronously* -- don't block
+                // this thread waiting for the user's response! After the user
+                // sees the explanation, try again to request the permission.
+            } else {
+                // No explanation needed; request the permission
+                ActivityCompat.requestPermissions(MainActivity.this,
+                        new String[]{Manifest.permission.CAMERA},
+                        PERMISSION_REQUEST_CAMERA);
+
+                // MY_PERMISSIONS_REQUEST_READ_CONTACTS is an
+                // app-defined int constant. The callback method gets the
+                // result of the request.
+            }
+        } else {
+            // Permission has already been granted
+        }
+
+    }
+    //Ask for permission to use camera and microphone
+    private void CheckForAudioPermission(){        // Here, thisActivity is the current activity
+        if (ContextCompat.checkSelfPermission(MainActivity.this,
+                Manifest.permission.RECORD_AUDIO)
+                != PackageManager.PERMISSION_GRANTED) {
+
+            // Permission is not granted
+            // Should we show an explanation?
+            if (ActivityCompat.shouldShowRequestPermissionRationale(MainActivity.this,
+                    Manifest.permission.RECORD_AUDIO)) {
+                // Show an explanation to the user *asynchronously* -- don't block
+                // this thread waiting for the user's response! After the user
+                // sees the explanation, try again to request the permission.
+            } else {
+                // No explanation needed; request the permission
+                ActivityCompat.requestPermissions(MainActivity.this,
+                        new String[]{Manifest.permission.RECORD_AUDIO},
+                        PERMISSION_REQUEST_AUDIO);
+
+                // MY_PERMISSIONS_REQUEST_READ_CONTACTS is an
+                // app-defined int constant. The callback method gets the
+                // result of the request.
+            }
+        } else {
+            // Permission has already been granted
+        }
+
+    }
 }
+
+
